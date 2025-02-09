b5db2a09125abee39fb2d2825ed5761f121e4a54
VARUN K <u1530331@lab2-9.eng.utah.edu>
2025-02-08 19:02:48 -0700

    Task1 completed

--- Src/lab2.c
+++ Src/lab2.c
@@ -1,13 +1,19 @@
 #include <stm32f0xx_hal.h>
 #include "main.h"
 #include "hal_gpio.h"
+#include "stm32f0xx.h" 
+
+void EXTI0_1_IRQHandler(void){
+    EXTI->PR = (1 << 0); //Clearing the flag
+    HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_8 | GPIO_PIN_9);
+}
 
 int lab2_main(void){
     HAL_Init();
     SystemClock_Config();
     __HAL_RCC_GPIOC_CLK_ENABLE(); 
 
-    GPIO_InitTypeDef initStr = {GPIO_PIN_6 | GPIO_PIN_7,
+    GPIO_InitTypeDef initStr = {GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9,
                                 GPIO_MODE_OUTPUT_PP,
                                 GPIO_SPEED_FREQ_LOW,
                                 GPIO_NOPULL};
@@ -15,7 +21,10 @@ int lab2_main(void){
     My_HAL_GPIO_Init(GPIOC, &initStr);
 
     My_HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
-    
+
+    //Button Interrupt
+    My_EXTI_INRPT();
+
     while (1)
     {
         HAL_Delay(400);
