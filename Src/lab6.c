bd78d2d5e470ac14e1d6e7aa7e6ca9a8824e6dc9
VARUN BHAVYA <varunbhavya@VARUNs-MacBook-Pro.local>
2025-03-24 12:22:46 -0600

    LAB6 wrote code, needs to be tested

---
+++ Src/lab6.c
@@ -0,0 +1,62 @@
+#include <stm32f0xx_hal.h>
+#include "main.h"
+#include "stm32f072xb.h"
+
+
+void led_init(){
+    GPIO_InitTypeDef initStr = {GPIO_PIN_6 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9,
+        GPIO_MODE_OUTPUT_PP,
+        GPIO_SPEED_FREQ_LOW,
+        GPIO_NOPULL};
+    RCC->AHBENR |= RCC_AHBENR_GPIOCEN;
+    HAL_GPIO_Init(GPIOC, &initStr);
+}
+
+void adc_init(){
+    //Configuring to use ADC_IN10 channel
+    //Set PC0 to AF0
+    GPIOC->MODER |= (3 << (2 * 0)); 
+    GPIOC->PUPDR &= ~(3 << (2 * 0));
+
+    //Clock Enable to ADC1
+    RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
+
+    //Calibration
+    ADC1->CFGR1 |= ADC_CFGR1_CONT;
+    ADC1->CFGR1 &= ~ADC_CFGR1_RES;
+    ADC1->CFGR1 |= ADC_CFGR1_RES_1;
+
+
+    if ((ADC1->CR & ADC_CR_ADEN) != 0)
+    {
+        ADC1->CR |= ADC_CR_ADDIS; 
+    }
+
+    while ((ADC1->CR & ADC_CR_ADEN) != 0){}
+
+    ADC1->CR |= ADC_CR_ADCAL;
+    while ((ADC1->CR & ADC_CR_ADCAL) != 0){}
+
+    ADC1->CFGR1 &= ~ADC_CFGR1_DMAEN;
+
+    if ((ADC1->ISR & ADC_ISR_ADRDY) != 0) 
+    {
+        ADC1->ISR |= ADC_ISR_ADRDY;
+    }
+    ADC1->CR |= ADC_CR_ADEN;
+    while ((ADC1->ISR & ADC_ISR_ADRDY) == 0){}
+}
+
+int lab6_main(void){
+    HAL_Init();
+    SystemClock_Config();
+    adc_init();
+
+    while(1){
+        uint32_t adc_value = ADC1->DR;
+        if (adc_value > 128){
+            HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
+        }
+    }
+    return 0;
+}
