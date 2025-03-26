7ea5daea7fb30299f3f35fe499f743d53bd30e49
VARUN BHAVYA <varunbhavya@VARUNs-MacBook-Pro.local>
2025-03-26 08:34:08 -0600

    ADC Task Done

--- Src/lab6.c
+++ Src/lab6.c
@@ -14,18 +14,17 @@ void led_init(){
 
 void adc_init(){
     //Configuring to use ADC_IN10 channel
-    //Set PC0 to AF0
-    GPIOC->MODER |= (3 << (2 * 0)); 
-    GPIOC->PUPDR &= ~(3 << (2 * 0));
+    GPIOC->MODER |= ((1 << 0) | (1 << 1)); 
 
     //Clock Enable to ADC1
     RCC->APB2ENR |= RCC_APB2ENR_ADC1EN;
 
+    
     //Calibration
     ADC1->CFGR1 |= ADC_CFGR1_CONT;
-    ADC1->CFGR1 &= ~ADC_CFGR1_RES;
-    ADC1->CFGR1 |= ADC_CFGR1_RES_1;
-
+    ADC1->CFGR1 &= ~ADC_CFGR1_RES; 
+    ADC1->CFGR1 |= ADC_CFGR1_RES_0; 
+    ADC1->CHSELR = ADC_CHSELR_CHSEL10;
 
     if ((ADC1->CR & ADC_CR_ADEN) != 0)
     {
@@ -35,6 +34,7 @@ void adc_init(){
     while ((ADC1->CR & ADC_CR_ADEN) != 0){}
 
     ADC1->CR |= ADC_CR_ADCAL;
+
     while ((ADC1->CR & ADC_CR_ADCAL) != 0){}
 
     ADC1->CFGR1 &= ~ADC_CFGR1_DMAEN;
@@ -43,35 +43,46 @@ void adc_init(){
     {
         ADC1->ISR |= ADC_ISR_ADRDY;
     }
+
     ADC1->CR |= ADC_CR_ADEN;
+
     while ((ADC1->ISR & ADC_ISR_ADRDY) == 0){}
+
+    //Start ADC
+    ADC1->CR |= ADC_CR_ADSTART;
 }
 
 
-void adc_led(uint32_t adc_value){ 
+void adc_led(uint32_t adc_value){
+    if (adc_value < 20){
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET); 
+    } 
     if (adc_value >= 20 && adc_value < 64){
         HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
     }
     if (adc_value >= 64 && adc_value < 128){
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
         HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
     }
     if (adc_value >= 128 && adc_value < 192){
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
         HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
     }
     if (adc_value >= 192){
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_7, GPIO_PIN_SET);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_8, GPIO_PIN_SET);
         HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
-        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET);
     }
 }
 
@@ -79,6 +90,7 @@ void adc_led(uint32_t adc_value){
 int lab6_main(void){
     HAL_Init();
     SystemClock_Config();
+    led_init();
     adc_init();
 
     HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_RESET);
@@ -90,6 +102,7 @@ int lab6_main(void){
 
     while(1){
         adc_value = ADC1->DR;
+        HAL_Delay(100);
         adc_led(adc_value);
     }
     return 0;
